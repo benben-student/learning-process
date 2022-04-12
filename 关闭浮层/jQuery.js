9228932e922c62f1412162cabc4a0bcafd2e560a
520-forever <a441832620@126.com>
2022-04-12 20:00:39 +0800

    add

--- 关闭浮层/jQuery.js
+++ 关闭浮层/jQuery.js
@@ -10,11 +10,14 @@
 
 $(clickMe).on('click',function(){
     $(popover).show()
-    $(document).one('click',function(){
-        console.log('document')
-        $(popover).hide()
-    })
-})
-$(wrapper).on('click',function(e){
-    e.stopPropagation()
+    console.log('show')
+    setTimeout(function(){
+        console.log('添加 one click')
+        $(document).one('click',function(){
+            console.log('我觉得这里不会执行')
+            console.log('hide')
+            $(popover).hide()
+        })
+    },0)
+  
 })
