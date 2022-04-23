3350cba20ab641acc2c5b575a69d0e3b5c294b94
520-forever <a441832620@126.com>
2022-04-23 18:54:24 +0800

    note

---
+++ 运行结果/practice.js
@@ -0,0 +1,46 @@
+//.join()
+var arrr=['1','2','3','a','b','c']
+var abc=arrr.join('-')
+console.log(abc)
+
+
+
+//.split()
+var str='123'
+var a=str.split('')//给所有加上''
+console.log(a)//"1""2""3"
+
+var b=str.split('',2)//返回前两个
+console.log(b)//'1''2'
+
+var str1='2:3:4'
+var c=str1.split(':')
+console.log(c)//将返回'2''3''4'
+// 用|也一样
+
+
+
+//[]转化为'' , ''转化为[]
+var obj=[1,2,3]
+var aa=obj.join('-')
+console.log(aa)
+var bb=aa.split('-')
+console.log(bb)
+var cc=aa.split('-').map(item=>Number(item))//箭头函数  相当于function(item){return  Number(item)}
+console.log(cc)
+
+
+
+
+
+
+
+
+//练习
+var arr2=[a,c,v,b,n]
+var aaa=arr2.join('-')
+console.log(aaa)
+var bbb=aaa.split('-')
+console.log(bbb)
+var ccc=aaa.split('-').map(item=>Number(item))
+console.log(ccc)
