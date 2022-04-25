f3f99e920243dddad9723bb8cb9c334e52d13b02
520-forever <a441832620@126.com>
2022-04-25 18:59:06 +0800

    add

--- 运行结果/周总结01.js
+++ 运行结果/周总结01.js
@@ -1,36 +1,59 @@
-一:主线
+//一:主线
 
-1.JSON:请求方式
+//1.请求方式
+//link请求
+var link=document.createElement('link')
+link.rel='stylesheet'
+link.href='/xxx'
+document.head.appendChild(link)
 
-link请求
+//image请求
+var image=document.createElement('img')
+image.src='/xxx'
+document.body.appendChild(image)
+image.onload=function(){
+    console.log('s')
+}
+image.onerror=function(){
+    console.log('f')
+}
 
-image请求
+//post请求
+         /*
+              <form action="/xxx" method=post>
+                <input type="password" name="password"></input>
+                <input type='submit'></input>
+              </form>
+          */
 
-post请求
+//get请求
+        /*
+        <form action="/xxx" method=get>
+             <input type="password" name="password">
+             <input type="submit">
+       </form>
+        */
+//2.AJAX原生ajax
 
-get请求
+//二：学习笔记
 
-2.AJAX原生ajax
+//1：箭头函数
 
-二：学习笔记
+//x => x * x相当于
 
-1：箭头函数
-
-x => x * x相当于
-
-  function (x) {
+  /*function (x) {
 
 ​    return x * x;
 
-  }
+  }*/
 
-2.把[1,2,3,4]转化为['1','2','3','4']，['1','2','3','4']转化为[1,2,3,4]
+//2.把[1,2,3,4]转化为['1','2','3','4']，['1','2','3','4']转化为[1,2,3,4]
 
 var obj=[1,2,3]//声明
 
 var aa=obj.join('-')//对数组里的数据加'-'
 
-console.log(aa)//['1'-'2'-'3-'4']
+console.log(aa)//['1'-'2'-'3'-'4']
 
 var bb=aa.split('-')
 
