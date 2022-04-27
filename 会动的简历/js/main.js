2e1b640f765a3698f682ccd11a774965009e8183
520-forever <a441832620@126.com>
2022-04-27 19:37:58 +0800

    practice

--- 会动的简历/js/main.js
+++ 会动的简历/js/main.js
@@ -1,4 +1,19 @@
-var result=`/*
+function writeCode(prefix, code, fn) {
+    let domCode = document.querySelector('#code')
+    domCode.innerHTML = prefix || ''
+    let n = 0
+    let id = setInterval(() => {
+        n += 1
+        domCode.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css)
+        styleTag.innerHTML = prefix + code.substring(0, n)
+        domCode.scrollTop = domCode.scrollHeight
+        if (n >= code.length) {
+            window.clearInterval(id)
+            fn.call()
+        }
+    }, 10)
+}
+var result = `/*
 *面试官你好，我是奔奔
 *我将以动画的形式来介绍我自己
 *只用文字介绍太单纯了
@@ -16,17 +31,81 @@ html{
    border:1px solid red;
    padding:16px;
 }
+#code{
+    transform: rotate(360deg);
+/*我需要一点代码高亮*/
+.token.selector {
+    color: black;
+  }
+  .token.property {
+    color: black;
+  }
+  .token.function {
+    color: black;
+  }
+  #code {
+    height: 100vh;
+    overflow: hidden;
+  }
+  .token.selector {
+    color: black;
+  }
+  .token.property {
+    color: black;
+  }
+  .token.function {
+    color: black;
+  }
+  #code {
+    height: 100vh;
+    overflow: hidden;
+  }
 `
-var n=0
-var id=setInterval(()=>{
-    n+=1
-    code.innerHTML=result.substring(0,n)
-    styleTag.innerHTML=result.substring(0,n)
-    console.log('一轮')
-    if(n>=result.length){
-       window.clearInterval(id)
-    }
-},100)
+var result2 = `
+#paper{
+    width:100px;
+    height:100px;
+    background:red;
+}
+`
+writeCode('', result, () => {//writeCode call the function
+    createPaper(() => {
+        writeCode(result, result2)
+    })
+})//定闹钟：50毫秒之后开始写第一行代码啊
+//1.定闹钟
+//2.writeCode返回
+//3.执行fn2()
+//4.闹钟时间到
+//5.写第一行代码
 
+
+
+function createPaper(fn) {
+    var paper = document.createElement('div')
+    paper.id = 'paper'
+    document.body.appendChild(paper)
+    fn.call()
+}
+function fn3(preResult) {
+    var result = `
+    #paper{
+        width:100px;
+        height:100px;
+        background:red;
+    }
+`
+}
+var n = 0
+var id = setInterval(() => {
+    n += 1
+    code.innerHTML = preResult + result.substring(0, n)
+    code.innerHTML =
+        Prism.highlight(code.innerHTML, Prism.languages.css)
+    styleTag.innerHTML = result.substring(0, n)
+    if (n >= result.length) {
+        window.clearInterval(id)
+    }
+}, 50)
 
 
