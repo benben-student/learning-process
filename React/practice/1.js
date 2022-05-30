e31c653073f227c0bf4c0acb83209b6d83b3570f
520-forever <a441832620@126.com>
2022-05-30 22:25:51 +0800

    practice

---
+++ React/practice/1.js
@@ -0,0 +1,70 @@
+function App(props){
+    return(
+        <div>
+            <Box1 name='frank'/>
+            <Box2 name="jack"/>
+        </div>
+    )
+}
+class Box1 extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            number:0
+        }
+    }
+    add(){
+         this.setState({
+             number:this.state.number+1
+         })
+    }
+    minus(){
+        this.setState({
+            number:this.state.number-1
+        })
+    }
+    render(){
+        return(
+            <div className='red'>
+                <span>{this.state.number}</span>
+                <button onClick={this.add.bind(this)}>+</button>
+                <button onClick={this.minus.bind(this)}>-</button>
+                {this.props.name}
+            </div>
+        )
+    }
+}
+class Box2 extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            number:0
+        }
+    }
+    add(){
+        this.setState({
+            number:this.state.number+2
+        })
+    }
+    minus(){
+        this.setState({
+            number:this.state.number-2
+        })
+    }
+    render(){
+        return(
+            <div className='red'>
+               <span>{this.state.number}</span>
+               <button onClick={this.add.bind(this)}>+</button>
+               <button onClick={this.minus.bind(this)}>-</button>
+            </div>
+        )
+    }
+}
+render()
+function render(){
+    ReactDOM.render(
+        <App/>,
+        document.querySelector('#root')
+    )
+}
