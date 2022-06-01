66c1223d752a9054ffb5050c96e87285d168ebc6
520-forever <a441832620@126.com>
2022-06-01 17:30:08 +0800

    add

---
+++ React/three/React.js
@@ -0,0 +1,122 @@
+﻿var money={
+    amount:1000000
+}
+class App extends React.Component{
+    changeMessage(){
+        this.setState({
+            message:'真好'
+        })
+    }
+    constructor(){
+        super()
+        this.state={
+           message:'你好'
+        }
+    }
+    render(){
+        return(
+            <div className="root">
+                <BigPapa/>
+                <YoungPapa/>
+            </div>
+        )
+    }
+}
+class BigPapa extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            money:money
+        }
+    }
+    render(){
+        return(
+        <div className="papa">大爸{this.state.money.amount}
+            <Son1/>
+            <Son2/>
+        </div>
+    )}
+    
+}
+class YoungPapa extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            money:money
+        }
+    }
+    render(){
+        return(
+            <div className="papa">二爸{this.state.money.amount}
+                <Son3/>
+                <Son4/>
+            </div>
+        )
+    }
+   
+}
+class Son1 extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            money:money
+        }
+    }
+    render(){
+        return(
+            <div className="son">儿子1{this.state.money.amount}</div>
+        )
+    }
+   
+}
+class Son2 extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            money:money
+        }
+    }
+    x(){
+        money.amount-=100
+        this.setState({
+            money:money
+        })
+    }
+    render(){
+        return(
+            <div className="son">儿子2{this.state.money.amount}
+            <button onClick={()=>{this.x()}}>消费</button>
+            </div>
+        )
+    }
+    
+}
+class Son3 extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            money:money
+        }
+    }
+    render(){
+        return(
+            <div className="son">儿子3 {this.state.money.amount}</div>
+        )
+    }
+    
+}
+class Son4 extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            money:money
+        }
+    }
+    render(){
+        return(
+            <div className="son"> 儿子4 {this.state.money.amount}</div>
+        )
+    }
+   
+}
+ReactDOM.render(<App/>,document.querySelector("#root"))
---
+++ React/three/main.js
@@ -0,0 +1,29 @@
+﻿class App extends React.Component{
+    changeMessage(){
+        this.setState({
+            message:'真好'
+        })
+    }
+    constructor(){
+        super()
+        this.state={
+           message:'你好'
+        }
+    }
+    render(){
+        return(
+            <div>hi
+                <Foo message={this.state.message}
+                fn={this.changeMessage.bind(this)}/>
+            </div>
+        )
+    }
+}
+function Foo(props){
+    return(
+        <p>得到的message是{props.message}
+            <button onClick={props.fn}>click</button>
+        </p>
+    )
+}
+ReactDOM.render(<App/>,document.querySelector("#root"))
